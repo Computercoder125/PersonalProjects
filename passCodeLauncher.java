b74a73531cd986a787be1d2c362451668c2977b0
Computercoder125 <sg10763150@sju.edu>
2024-11-27 17:59:38 -0500

    Add files via upload

---
+++ passCodeLauncher.java
@@ -0,0 +1,10 @@
+public class  passCodeLauncher
+{
+    public static void main(String[] args)
+    {
+        passCode launch = new passCode("2321"); //accessing the main passCode class
+
+        launch.playGame();
+
+    }
+}
